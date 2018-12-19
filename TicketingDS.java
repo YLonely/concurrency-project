83beb7e3a407e66b20b69290f675c41f5c7e9117
Lw <loneybw@gmail.com>
2018-12-19 23:22:07 +0800

    Change file location

---
+++ TicketingDS.java
@@ -0,0 +1,21 @@
+package ticketingsystem;
+
+public class TicketingDS implements TicketingSystem {
+
+	// ToDo
+	public TicketingDS(int routeNum, int coachNum, int seatNum, int stationNum, int threadNum) {
+
+	}
+
+	public Ticket buyTicket(String passenger, int route, int departure, int arrival) {
+
+	}
+
+	public int inquiry(int route, int departure, int arrival) {
+
+	}
+
+	public boolean refundTicket(Ticket ticket) {
+
+	}
+}
--- src/ticketingsystem/TicketingDS.java
+++
@@ -1,7 +0,0 @@
-package ticketingsystem;
-
-public class TicketingDS implements TicketingSystem {
-		
-	//ToDo
-
-}
