aa610cdf7f420c0c5ed02ef683a84204b7d1d303
Lw <loneybw@gmail.com>
2018-12-22 10:25:15 +0800

    Before Test

--- ticketingsystem/Test.java
+++ ticketingsystem/Test.java
@@ -1,10 +1,82 @@
 package ticketingsystem;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Test {
+	private final static int ROUTE_NUM = 5;
+	private final static int COACH_NUM = 8;
+	private final static int SEAT_NUM = 100;
+	private final static int STATION_NUM = 10;
+
+	private final static int TEST_NUM = 1000000;
+	private final static int refund = 10;
+	private final static int buy = 40;
+	private final static int query = 100;
+
+	static String passengerName() {
+		Random rand = new Random();
+		long uid = rand.nextInt(TEST_NUM);
+		return "passenger" + uid;
+	}
 
 	public static void main(String[] args) throws InterruptedException {
+		final int[] threadNums = { 4, 8, 16, 32, 64, 128 };
+		int p;
+		for (p = 0; p < 6; ++p) {
+			final TicketingDS tds = new TicketingDS(ROUTE_NUM, COACH_NUM, SEAT_NUM, STATION_NUM, threadNums[p]);
+			Thread[] threads = new Thread[threadNums[p]];
+			for (int i = 0; i < threadNums[p]; i++) {
+				threads[i] = new Thread(new Runnable() {
+					public void run() {
+						Random rand = new Random();
+						Ticket ticket = new Ticket();
+						ArrayList<Ticket> soldTicket = new ArrayList<Ticket>();
+						for (int i = 0; i < TEST_NUM; i++) {
+							int sel = rand.nextInt(query);
+							if (0 <= sel && sel < refund && soldTicket.size() > 0) { // refund ticket 0-10
+								int select = rand.nextInt(soldTicket.size());
+								if ((ticket = soldTicket.remove(select)) != null) {
+									if (tds.refundTicket(ticket)) {
+										;
+									} else {
+										System.out.println("ErrOfRefund1");
+									}
+								} else {
+									System.out.println("ErrOfRefund2");
+								}
+							} else if (refund <= sel && sel < buy) { // buy ticket 10-40
+								String passenger = passengerName();
+								int route = rand.nextInt(ROUTE_NUM) + 1;
+								int departure = rand.nextInt(STATION_NUM - 1) + 1;
+								int arrival = departure + rand.nextInt(STATION_NUM - departure) + 1;
+								if ((ticket = tds.buyTicket(passenger, route, departure, arrival)) != null) {
+									soldTicket.add(ticket);
+								} else {
+									;
+								}
+							} else if (buy <= sel && sel < query) { // inquiry ticket 40-100
+								int route = rand.nextInt(ROUTE_NUM) + 1;
+								int departure = rand.nextInt(STATION_NUM - 1) + 1;
+								int arrival = departure + rand.nextInt(STATION_NUM - departure) + 1;
+								int leftTicket = tds.inquiry(route, departure, arrival);
+							}
+						}
+					}
+				});
+			}
+			long start = System.currentTimeMillis();
+			for (int i = 0; i < threadNums[p]; ++i)
+				threads[i].start();
 
-		final TicketingDS tds = new TicketingDS();
-		// ToDo
+			for (int i = 0; i < threadNums[p]; i++) {
+				threads[i].join();
+			}
+			long time = System.currentTimeMillis() - start; // 单位是ms
+			long t = (long) (threadNums[p] * TEST_NUM / (double) (time)) * 1000; // 1000是从ms转换为s
+			System.out.println(
+					String.format("ThreadNum: %d TotalTime(ms): %d ThroughOut(t/s): %d", threadNums[p], time, t));
+
+		}
 	}
 }
--- ticketingsystem/Trace.java
+++ ticketingsystem/Trace.java
@@ -5,105 +5,112 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class ThreadId {
-    // Atomic integer containing the next thread ID to be assigned
-    private static final AtomicInteger nextId = new AtomicInteger(0);
+	// Atomic integer containing the next thread ID to be assigned
+	private static final AtomicInteger nextId = new AtomicInteger(0);
 
-    // Thread local variable containing each thread's ID
-    private static final ThreadLocal<Integer> threadId =
-        new ThreadLocal<Integer>() {
-            @Override protected Integer initialValue() {
-                return nextId.getAndIncrement();
-        }
-    };
+	// Thread local variable containing each thread's ID
+	private static final ThreadLocal<Integer> threadId = new ThreadLocal<Integer>() {
+		@Override
+		protected Integer initialValue() {
+			return nextId.getAndIncrement();
+		}
+	};
 
-    // Returns the current thread's unique ID, assigning it if necessary
-    public static int get() {
-        return threadId.get();
-    }
+	// Returns the current thread's unique ID, assigning it if necessary
+	public static int get() {
+		return threadId.get();
+	}
 }
 
 public class Trace {
 	final static int threadnum = 1; // concurrent thread number
-	final static int routenum = 3; // route is designed from 1 to 3
-	final static int coachnum = 3; // coach is arranged from 1 to 5
-	final static int seatnum = 3; // seat is allocated from 1 to 20
-	final static int stationnum = 3; // station is designed from 1 to 5
+	final static int routenum = 1; // route is designed from 1 to 3
+	final static int coachnum = 1; // coach is arranged from 1 to 5
+	final static int seatnum = 100; // seat is allocated from 1 to 20
+	final static int stationnum = 2; // station is designed from 1 to 5
 
-	final static int testnum = 40000;
-	final static int retpc = 30; // return ticket operation is 10% percent
-	final static int buypc = 60; // buy ticket operation is 30% percent
-	final static int inqpc = 100; //inquiry ticket operation is 60% percent
-	
+	final static int testnum = 400;
+	final static int retpc = 0; // return ticket operation is 10% percent
+	final static int buypc = 80; // buy ticket operation is 30% percent
+	final static int inqpc = 100; // inquiry ticket operation is 60% percent
+
 	static String passengerName() {
 		Random rand = new Random();
 		long uid = rand.nextInt(testnum);
-		return "passenger" + uid; 
+		return "passenger" + uid;
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-        
-		 
+
 		Thread[] threads = new Thread[threadnum];
-		
+
 		final TicketingDS tds = new TicketingDS(routenum, coachnum, seatnum, stationnum, threadnum);
-		
 
-	    for (int i = 0; i< threadnum; i++) {
-	    	threads[i] = new Thread(new Runnable() {
-                public void run() {
-            		Random rand = new Random();
-                	Ticket ticket = new Ticket();
-            		ArrayList<Ticket> soldTicket = new ArrayList<Ticket>();
-            		
-            		//System.out.println(ThreadId.get());
-            		for (int i = 0; i < testnum; i++) {
-            			int sel = rand.nextInt(inqpc);
-            			if (0 <= sel && sel < retpc && soldTicket.size() > 0) { // return ticket
-            				int select = rand.nextInt(soldTicket.size());
-           				if ((ticket = soldTicket.remove(select)) != null) {
-            					if (tds.refundTicket(ticket)) {
-            						System.out.println("TicketRefund" + " " + ticket.tid + " " + ticket.passenger + " " + ticket.route + " " + ticket.coach  + " " + ticket.departure + " " + ticket.arrival + " " + ticket.seat);
-            						System.out.flush();
-            					} else {
-            						System.out.println("ErrOfRefund");
-            						System.out.flush();
-            					}
-            				} else {
-            					System.out.println("ErrOfRefund");
-        						System.out.flush();
-            				}
-            			} else if (retpc <= sel && sel < buypc) { // buy ticket
-            				String passenger = passengerName();
-            				int route = rand.nextInt(routenum) + 1;
-            				int departure = rand.nextInt(stationnum - 1) + 1;
-            				int arrival = departure + rand.nextInt(stationnum - departure) + 1; // arrival is always greater than departure
-            				if ((ticket = tds.buyTicket(passenger, route, departure, arrival)) != null) {
-            					soldTicket.add(ticket);
-            					System.out.println("TicketBought" + " " + ticket.tid + " " + ticket.passenger + " " + ticket.route + " " + ticket.coach + " " + ticket.departure + " " + ticket.arrival + " " + ticket.seat);
-        						System.out.flush();
-            				} else {
-            					System.out.println("TicketSoldOut" + " " + route+ " " + departure+ " " + arrival);
-        						System.out.flush();
-            				}
-            			} else if (buypc <= sel && sel < inqpc) { // inquiry ticket
-            				
-            				int route = rand.nextInt(routenum) + 1;
-            				int departure = rand.nextInt(stationnum - 1) + 1;
-            				int arrival = departure + rand.nextInt(stationnum - departure) + 1; // arrival is always greater than departure
-            				int leftTicket = tds.inquiry(route, departure, arrival);
-            				System.out.println("RemainTicket" + " " + leftTicket + " " + route+ " " + departure+ " " + arrival);
-    						System.out.flush();  
-    						         			
-            			}
-            		}
+		for (int i = 0; i < threadnum; i++) {
+			threads[i] = new Thread(new Runnable() {
+				public void run() {
+					Random rand = new Random();
+					Ticket ticket = new Ticket();
+					ArrayList<Ticket> soldTicket = new ArrayList<Ticket>();
 
-                }
-            });
-              threads[i].start();
- 	    }
-	
-	    for (int i = 0; i< threadnum; i++) {
-	    	threads[i].join();
-	    }		
+					// System.out.println(ThreadId.get());
+					for (int i = 0; i < testnum; i++) {
+						int sel = rand.nextInt(inqpc);
+						if (0 <= sel && sel < retpc && soldTicket.size() > 0) { // return ticket
+							int select = rand.nextInt(soldTicket.size());
+							if ((ticket = soldTicket.remove(select)) != null) {
+								if (tds.refundTicket(ticket)) {
+									System.out.println("TicketRefund" + " " + ticket.tid + " " + ticket.passenger + " "
+											+ ticket.route + " " + ticket.coach + " " + ticket.departure + " "
+											+ ticket.arrival + " " + ticket.seat);
+									System.out.flush();
+								} else {
+									System.out.println("ErrOfRefund");
+									System.out.flush();
+								}
+							} else {
+								System.out.println("ErrOfRefund");
+								System.out.flush();
+							}
+						} else if (retpc <= sel && sel < buypc) { // buy ticket
+							String passenger = passengerName();
+							int route = rand.nextInt(routenum) + 1;
+							int departure = rand.nextInt(stationnum - 1) + 1;
+							int arrival = departure + rand.nextInt(stationnum - departure) + 1; // arrival is always
+																								// greater than
+																								// departure
+							if ((ticket = tds.buyTicket(passenger, route, departure, arrival)) != null) {
+								soldTicket.add(ticket);
+								System.out.println("TicketBought" + " " + ticket.tid + " " + ticket.passenger + " "
+										+ ticket.route + " " + ticket.coach + " " + ticket.departure + " "
+										+ ticket.arrival + " " + ticket.seat);
+								System.out.flush();
+							} else {
+								System.out.println("TicketSoldOut" + " " + route + " " + departure + " " + arrival);
+								System.out.flush();
+							}
+						} else if (buypc <= sel && sel < inqpc) { // inquiry ticket
+
+							int route = rand.nextInt(routenum) + 1;
+							int departure = rand.nextInt(stationnum - 1) + 1;
+							int arrival = departure + rand.nextInt(stationnum - departure) + 1; // arrival is always
+																								// greater than
+																								// departure
+							int leftTicket = tds.inquiry(route, departure, arrival);
+							System.out.println(
+									"RemainTicket" + " " + leftTicket + " " + route + " " + departure + " " + arrival);
+							System.out.flush();
+
+						}
+					}
+
+				}
+			});
+			threads[i].start();
+		}
+
+		for (int i = 0; i < threadnum; i++) {
+			threads[i].join();
+		}
 	}
 }
